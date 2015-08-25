7887d17c8183d326b12f2f2d3d138c9096617e66
Kate Milrud <kmilrud@yahoo.com>
2015-08-24 21:39:35 -0700

    Initial commit

---
+++ src/main/java/core/Title_Validation_PF.java
@@ -0,0 +1,54 @@
+package core;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Title_Validation_PF {
+
+	public static void main(String[] args) {
+		String test_case_id = null;
+		String url = null;
+		String title_expected = null;
+		
+		Properties properties = new Properties();
+		
+		try {
+		properties.load(new FileInputStream("./src/main/resources/Test.properties"));
+		
+			test_case_id = properties.getProperty("test_case_id");
+			url = properties.getProperty("url");
+			title_expected = properties.getProperty("title_expected");
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+		}
+		
+//		System.out.println(test_case_id);
+//		System.out.println(url);
+//		System.out.println(title_expected);
+		
+		WebDriver driver = new FirefoxDriver();  // Version 1.3 :: Firefox
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		String title_actual = driver.getTitle();
+		System.out.println("");
+		System.out.println("Test Case ID: \t\t" + test_case_id);
+		System.out.println("URL: \t\t\t" + url);
+		System.out.println("Title Expected: \t" + title_expected);
+		System.out.println("Title Actual: \t\t" + title_actual);
+		if (title_expected.equals(title_actual)) {
+			System.out.println("Test Case Result: \t" + "Passed");
+		} else  {
+			System.out.println("Test Case Result: \t" + "Failed");
+		}
+		driver.quit();	
+
+	}
+
+}
